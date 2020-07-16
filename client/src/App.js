150b2fc050a1709504556d54b7a59a9a87b3481a
Ihab-AL <ihab.alazzawi@outlook.com>
2020-07-15 22:52:49 -0500

    remove unnecessary id

--- client/src/App.js
+++ client/src/App.js
@@ -3,30 +3,30 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import Nav from "./components/Nav/nav";
-import "./App.css"
+import "./App.css";
 import home from "./components/Home/home";
 import login from "./components/Login/login";
-import NoMatch from "../src/pages/NoMatch"
-import Levels from "../src/pages/Levels"
+import NoMatch from "../src/pages/NoMatch";
+import Levels from "../src/pages/Levels";
 import store from "./store";
 import { Provider } from "react-redux";
 
 function App() {
   return (
     <Provider store={store}>
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/login" component={login} />
-          <Route exact path="/home" component={home} />
-          <Route exact path="/dashboard/:id" component={Levels} />
-          {/* <Route exact path="/books" component={Books} /> */}
-          {/* <Route exact path="/books/:id" component={Detail} /> */}
-          <Route component={NoMatch} />
-        </Switch>
-      </div>
-    </Router>
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path="/login" component={login} />
+            <Route exact path="/home" component={home} />
+            <Route exact path="/dashboard" component={Levels} />
+            {/* <Route exact path="/books" component={Books} /> */}
+            {/* <Route exact path="/books/:id" component={Detail} /> */}
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
     </Provider>
   );
 }
