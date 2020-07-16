d421cf009bf8b9eaa0aad1c80b36289834696991
Ihab-AL <ihab.alazzawi@outlook.com>
2020-07-15 22:57:40 -0500

    refactor auth routes

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,77 +1,55 @@
-const router = require("express");
+const router = require("express").Router();
 const passport = require("passport");
-const app = router();
 
 // // auth logout
-// router.route("/logout").get((req, res) => {
-//   // handle with passport
-//   let redirectPath =
-//     process.env.NODE_ENV === "production"
-//       ? "https://esl-final-project.herokuapp.com/"
-//       : "http://localhost:3000/";
-//   req.logout();
-//   res.redirect(redirectPath);
-// });
+router.route("/logout").get((req, res) => {
+  // handle with passport
+  let redirectPath =
+    process.env.NODE_ENV === "production"
+      ? "https://esl-final-project.herokuapp.com/"
+      : "http://localhost:3000/";
+  req.logout();
+  res.redirect(redirectPath);
+});
 
 // auth with google
-// router.route("/google").get(
-//   passport.authenticate("google", {
-//     scope: ["openid", "email", "profile"],
-//   })
-// );
+router.route("/google").get(
+  passport.authenticate("google", {
+    scope: ["openid", "email", "profile"],
+  })
+);
 
 // auth with facebook
-// router.route("/facebook").get(
-//   passport.authenticate("facebook", {
-//     scope: ["email"],
-//   })
-// );
+router.route("/facebook").get(
+  passport.authenticate("facebook", {
+    scope: ["email"],
+  })
+);
 
 // callback route for google to redirect to
-// router
-//   .route("/google/redirect")
-//   .get(passport.authenticate("google"), (req, res) => {
-//     let redirectPath = "/dashboard";
-//     res.redirect(redirectPath);
-//   });
+router
+  .route("/google/redirect")
+  .get(passport.authenticate("google"), (req, res) => {
+    let redirectPath =
+      process.env.NODE_ENV === "production"
+        ? "https://esl-final-project.herokuapp.com/dashboard"
+        : "http://localhost:3000/dashboard";
+    res.redirect(redirectPath);
+  });
 
 // callback route for facebook to redirect to
-// router
-//   .route("/facebook/redirect")
-//   .get(passport.authenticate("facebook"), (req, res) => {
-//     let redirectPath = "/dashboard";
-//     res.redirect(redirectPath);
-//   });
-
-// router.route("/current_user", (req, res) => {
-//   res.send(req.user);
-// });
-
-// module.exports = router;
-
-
-module.exports = app => {
-  app.get(
-    "/auth/google",
-    passport.authenticate("google", {
-      scope: [
-        "https://www.googleapis.com/auth/userinfo.profile",
-        "https://www.googleapis.com/auth/userinfo.email"
-      ]
-    })
-  );
-  app.get(
-    "/auth/google/callback",
-    passport.authenticate("google"),
-    (req, res) => {
-      res.redirect("/dashboard");
-    }
-  );
-  app.get("/api/logout", (req, res) => {
-    req.logout();
-    res.redirect("/home");
+router
+  .route("/facebook/redirect")
+  .get(passport.authenticate("facebook"), (req, res) => {
+    let redirectPath =
+      process.env.NODE_ENV === "production"
+        ? "https://esl-final-project.herokuapp.com/"
+        : "http://localhost:3000/dashboard";
+    res.redirect(redirectPath);
   });
-  app.get("/api/current_user", (req, res) => {
-    res.send(req.user);
+
+  router.get("/current_user", (req, res) => {
+    res.json(req.user);
   });
-};
+
+module.exports = router;
