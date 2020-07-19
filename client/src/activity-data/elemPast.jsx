94ef696ae45de44149d2283e53e8b932cf3f6b43
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-07-19 11:41:06 -0500

    convert json questions to jsx files

---
+++ client/src/activity-data/elemPast.jsx
@@ -0,0 +1,146 @@
+export const quiz = {
+    "quizTitle": "Elementary Past Tenses",
+    "quizSynopsis": "An exercise to check your knowledge of past simple and present continuous.",
+    "questions": [
+      {
+        "messageForCorrectAnswer": "Great job!",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "point": "1",
+        "questionType": "text",
+        "question": "Georgette  __________ 22 last year.",
+        "answers": [
+          "was",
+          "am",
+          "were",
+          "have"
+        ],
+        "correctAnswer": "1"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "Your clothes are nice.  __________ they expensive?",
+        "answers": [
+          "Was",
+          "Am",
+          "Were",
+          "Have"
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "Billy  __________ to stop his sister from pulling his hair.",
+        "answers": [
+          "try",
+          "tried",
+          "did try",
+          "has try"
+        ],
+        "correctAnswer": "2"
+      },
+      {
+        "questionType": "text",
+        "messageForCorrectAnswer": "Great job!",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "point": "1",
+        "question": "We  __________ the party last night.",
+        "answers": [
+          "enjoy",
+          "enjoyed",
+          "have",
+          "has"
+        ],
+        "correctAnswer": "2"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "Jean  __________ Scott from going home.",
+        "answers": [
+          "stop",
+          "stopped",
+          "stoped",
+          "stooped"
+        ],
+        "correctAnswer": "2"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "It  __________ all morning.",
+        "answers": [
+          "rain",
+          "reigned",
+          "rained",
+          "reign"
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "Gina  __________ for her dad through high school.",
+        "answers": [
+          "worked",
+          "work",
+          "had worked",
+          "have worked"
+        ],
+        "correctAnswer": "1"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "I  __________ a horror movie last night.",
+        "answers": [
+          "went",
+          "watch",
+          "watched",
+          "go"
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "Dallas __________ to bed hours ago.",
+        "answers": [
+          "go",
+          "gone",
+          "did",
+          "went"
+        ],
+        "correctAnswer": "4"
+      },
+      {
+        "questionType": "text",
+        "messageForCorrectAnswer": "Great job!",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "point": "1",
+        "question": "Bill and Joseph __________ on an adventure.",
+        "answers": [
+          "had",
+          "have",
+          "gone",
+          "went"
+        ],
+        "correctAnswer": "4"
+      }
+    ]
+  }
