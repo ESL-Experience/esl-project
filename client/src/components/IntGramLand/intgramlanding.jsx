766721fa63be1ba51e75a76c672717f398235339
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-07-19 16:01:22 -0500

    finish int gram land page and int grammar quizzes

---
+++ client/src/components/IntGramLand/intgramlanding.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class IntGramLand extends Component {
+    render() {
+        return (
+            <div>
+            <h1 style={{color:"white"}}>ElmGramLand</h1>
+        </div>
+        )
+    }
+}
---
+++ client/src/pages/IntFutrQuiz.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import Button from "../components/Button/Button";
+import { quiz } from "../activity-data/intFutr";
+import Quiz from "react-quiz-component";
+import { setCurrentUser } from "../actions/auth-actions";
+import { connect } from "react-redux";
+import "../components/Quiz/quiz.css";
+
+class generateQuiz extends Component {
+  state = {
+    user: {},
+  };
+
+  async componentDidMount() {
+    await this.props.setCurrentUser();
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { isComplete: false };
+  }
+
+  render() {
+    const onCompleteAction = (obj) => {
+      const title = quiz.quizTitle
+      const date = Date.now()
+      let quizData = {[title]: 
+        {dateTaken: date, actRes: obj}}
+      const { user } = this.props.auth;
+      console.log(quizData);
+      console.log(user);
+      return (
+        <div>
+          <div className="col-12 text-center">
+              <Button
+                onClick={() => this.props.history.push('/IntLanding')}
+                type="button"
+                buttonStyle="btn--yellow--outline"
+                buttonSize="btn--medium"
+              >
+                Home
+              </Button>
+          </div>
+        </div>
+      );
+    };
+
+    return (
+      <div>
+        <Quiz
+          quiz={quiz}
+          onComplete={onCompleteAction}
+          shuffle={true}
+          continueTillCorrect={false}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+export default connect(mapStateToProps, { setCurrentUser })(generateQuiz);
