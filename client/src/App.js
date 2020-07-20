8b18b4cb95f10504bdeada8800f217eddc970f8c
Kortnie Heidel <kortnie.evans@gmail.com>
2020-07-20 12:36:36 -0400

    cleans up unused/unneeded code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, withRouter, BrowserRouter, useHistory } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
 import Nav from "./components/Nav/nav";
 import "./App.css";
 // import home from "./components/Home/home";
@@ -74,25 +72,5 @@ const App = () => (
   </BrowserRouter>
   </Provider>
 )
-
-// function App() {
-//   return (
-//     <Provider store={store}>
-//       <Router>
-//         <div>
-//           <Nav />
-//           <Switch>
-//             <Route exact path="/login" component={login} />
-//             <Route exact path="/home" component={home} />
-//             <Route exact path="/dashboard" component={Levels} />
-//             {/* <Route exact path="/books" component={Books} /> */}
-//             {/* <Route exact path="/books/:id" component={Detail} /> */}
-//             <Route component={NoMatch} />
-//           </Switch>
-//         </div>
-//       </Router>
-//     </Provider>
-//   );
-// }
 
 export default App;
--- client/src/components/Login/login.jsx
+++ client/src/components/Login/login.jsx
@@ -1,38 +1,32 @@
-import React from 'react';
-import './login.css'
-import logo from '../../images/logo.png'
+import React from "react";
+import "./login.css";
+import logo from "../../images/logo.png";
 import { FacebookLoginButton } from "react-social-login-buttons";
 import { GoogleLoginButton } from "react-social-login-buttons";
 
-
- let googlePath = (process.env.NODE_ENV === "production") ? "https://esl-final-project.herokuapp.com/auth/google" : "http://localhost:3001/auth/google"
- let facebookPath = (process.env.NODE_ENV === "production") ? "https://esl-final-project.herokuapp.com/auth/facebook" : "http://localhost:3001/auth/facebook"
-
-
+let googlePath =
+  process.env.NODE_ENV === "production"
+    ? "https://esl-final-project.herokuapp.com/auth/google"
+    : "http://localhost:3001/auth/google";
+let facebookPath =
+  process.env.NODE_ENV === "production"
+    ? "https://esl-final-project.herokuapp.com/auth/facebook"
+    : "http://localhost:3001/auth/facebook";
 
 const container = () => {
   return (
     <div>
       <img className="logo" src={logo} />
       <h1>Login</h1>
-    <div
-    id="container"
-    className="col-12 text-center">
-      <a  
-        className="nav-link"
-        as='a' href={googlePath}>
+      <div id="container" className="col-12 text-center">
+        <a className="nav-link" as="a" href={googlePath}>
           <GoogleLoginButton></GoogleLoginButton>
-        {/* <button id="google" type="button" className="btn btn-gplus btn-danger"><i className="fab fa-google-plus-g pr-1">Login with Google</i></button> */}
-      </a>
-      <a  
-        className="nav-link"
-        as='a' href={facebookPath}>
+        </a>
+        <a className="nav-link" as="a" href={facebookPath}>
           <FacebookLoginButton></FacebookLoginButton>
-        {/* <button id="facebook" type="button" className="btn btn-facebook btn-danger"><i className="fab fa-facebook pr-1">Login with Facebook</i></button> */}
-      </a>
-        </div>
+        </a>
+      </div>
     </div>
-
   );
 };
 
