84c039acaa7aa8da0d4e28b0b8fd19b1c5fb4a81
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-07-20 20:04:03 -0500

    complete route and quiz for elm school vocabulary

---
+++ client/src/activity-data/elmSchoolVoc.jsx
@@ -0,0 +1,216 @@
+export const quiz = {
+    "quizTitle": "School Vocabulary",
+    "quizSynopsis": "See how many vocabulary words you get right about school.",
+    "questions": [
+      {
+        "questionType": "text",
+        "question": "Mr. Riley __________ us mathematics.",
+        "answers": [
+          "teach",
+          "teaches",
+          "wrote",
+          "write"
+        ],
+        "correctAnswer": "2",
+        "messageForCorrectAnswer": "Good job!",
+        "messageForInorrectAnswer": "Sorry, you are incorrect. ",
+        "point": "1"
+      },
+      {
+        "questionType": "text",
+        "messageForInorrectAnswer": "Sorry, you are incorrect. ",
+        "messageForCorrectAnswer": "Great job!",
+        "point": "1",
+        "question": "Children go to __________ to be taught lessons.",
+        "answers": [
+          "school",
+          "pool",
+          "exam",
+          "course"
+        ],
+        "correctAnswer": "1"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect. ",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "If you pass your final __________ you get a degree.",
+        "answers": [
+          "exams",
+          "pool",
+          "teach",
+          "computer"
+        ],
+        "correctAnswer": "1"
+      },
+      {
+        "questionType": "text",
+        "messageForInorrectAnswer": "Sorry, you are incorrect. ",
+        "messageForCorrectAnswer": "Great job!",
+        "point": "1",
+        "question": "You use this to write with.",
+        "answers": [
+          "eraser",
+          "pencil",
+          "notebook",
+          "desk"
+        ],
+        "correctAnswer": "2"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect. ",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "We learn about the world and maps in this class.",
+        "answers": [
+          "English",
+          "PE",
+          "Geography",
+          "Chemistry"
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect. ",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "We learn about the way things work in this class.",
+        "answers": [
+          "English",
+          "Physics",
+          "History",
+          "PE"
+        ],
+        "correctAnswer": "2"
+      },
+      {
+        "questionType": "text",
+        "messageForInorrectAnswer": "Sorry, you are incorrect. ",
+        "messageForCorrectAnswer": "Great job!",
+        "point": "1",
+        "question": "You get exercise in this class.",
+        "answers": [
+          "English",
+          "Physics",
+          "History",
+          "PE"
+        ],
+        "correctAnswer": "4"
+      },
+      {
+        "questionType": "text",
+        "messageForInorrectAnswer": "Sorry, you are incorrect. ",
+        "point": "1",
+        "messageForCorrectAnswer": "Great job!",
+        "question": "Harry does well at school. He finds it easy to __________.",
+        "answers": [
+          "sleep",
+          "learn",
+          "eat",
+          "write"
+        ],
+        "correctAnswer": "2"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect. ",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "Belinda always does her __________.",
+        "answers": [
+          "eat",
+          "physics",
+          "homework",
+          "pool"
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "messageForInorrectAnswer": "Sorry, you are incorrect. ",
+        "point": "1",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "You use this to type out your essays.",
+        "answers": [
+          "computer",
+          "calculator",
+          "paper",
+          "book"
+        ],
+        "correctAnswer": "1"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "At the end of this course, you will __________.",
+        "answers": [
+          "eat",
+          "sleep",
+          "graduate",
+          "read"
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "While at school, Sue learns to __________.",
+        "answers": [
+          "sleep",
+          "eat",
+          "swim",
+          "read"
+        ],
+        "correctAnswer": "4"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "You use this to sit at and to write on while at school.",
+        "answers": [
+          "toothpaste",
+          "desk",
+          "textbook",
+          "paper"
+        ],
+        "correctAnswer": "2"
+      },
+      {
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "point": "1",
+        "questionType": "text",
+        "question": "This is what you use to write on with a pencil or pen.",
+        "answers": [
+          "CD",
+          "DVD Player",
+          "Paper",
+          "Ruler"
+        ],
+        "correctAnswer": "3"
+      },
+      {
+        "point": "1",
+        "messageForInorrectAnswer": "Sorry, you are incorrect.",
+        "messageForCorrectAnswer": "Great job!",
+        "questionType": "text",
+        "question": "This is the class where you learn about language and vocabulary.",
+        "answers": [
+          "PE",
+          "English",
+          "Geography",
+          "Physics"
+        ],
+        "correctAnswer": "2"
+      }
+    ]
+  }
