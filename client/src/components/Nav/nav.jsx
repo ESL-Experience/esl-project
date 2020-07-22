fbb5a14e5e6af577b21ca323241f94102f375985
John Pendergrass <john.pendergrass@me.com>
2020-07-22 11:18:31 -0500

    made user img circle, centered name and nav items

--- client/src/components/Nav/nav.jsx
+++ client/src/components/Nav/nav.jsx
@@ -1,56 +1,62 @@
-import React, { Component } from "react";
-import logo from "./eng-exp-logo-wht-bg-no-txt.png";
-import "./nav.css";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import { setCurrentUser } from "../../actions/auth-actions";
+import React, { Component } from 'react'
+import logo from './eng-exp-logo-wht-bg-no-txt.png'
+import './nav.css'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { setCurrentUser } from '../../actions/auth-actions'
 
 class nav extends Component {
-  render() {
-    console.log(this.props.auth.user);
-    const { user } = this.props.auth;
+  render () {
+    console.log(this.props.auth.user)
+    const { user } = this.props.auth
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-none">
-          <a className="navbar-brand text-white" href="#">
-            <img src= { logo } height="250" width="250"/>
-          </a> <span className="title text-white">English Experience </span>
+        <nav className='navbar navbar-expand-lg navbar-light bg-none'>
+          <a className='navbar-brand text-white' href='#'>
+            <img src={logo} height='250' width='250' />
+          </a>{' '}
+          <span className='title text-white'>English Experience </span>
           <button
-            className="navbar-toggler navbar-dark"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+            className='navbar-toggler navbar-dark'
+            type='button'
+            data-toggle='collapse'
+            data-target='#navbarNavAltMarkup'
+            aria-controls='navbarNavAltMarkup'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <div className="collapse navbar-collapse " id="navbarNavAltMarkup">
-            <div className="navbar-nav ml-auto">
-          <img src= { user.thumbnail} height="40" width="40" className="user-img"/>
-          <span className="user text-white">{user.displayName}</span>
+          <div className='collapse navbar-collapse ' id='navbarNavAltMarkup'>
+            <div className='navbar-nav ml-auto'>
+              <img
+                src={user.thumbnail}
+                height='40'
+                width='40'
+                className='user-img rounded-circle'
+              />
+              <span className='user text-white'>{user.displayName}</span>
               {/* <Link to="/home" className="nav-item nav-link active text-white align-right">
                 Home <span className="sr-only">(current)</span>
               </Link> */}
-              <Link to="/dashboard" className="nav-item nav-link text-white" >
+              <Link to='/dashboard' className='nav-item nav-link text-white'>
                 Levels
               </Link>
               {/* <Link to="/Activity" className="nav-item nav-link text-white">
                 Activities
               </Link> */}
-              <Link to="/"className="nav-item nav-link text-white" >
+              <Link to='/' className='nav-item nav-link text-white'>
                 Log Out
               </Link>
             </div>
           </div>
         </nav>
       </div>
-    );
+    )
   }
 }
 
-const mapStateToProps = (state) => ({
-  auth: state.auth,
-});
-export default connect(mapStateToProps, { setCurrentUser })(nav);
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+export default connect(mapStateToProps, { setCurrentUser })(nav)
